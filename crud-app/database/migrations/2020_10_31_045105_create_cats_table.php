a14847404e23db3e2a8db4cae620d730896ab0b2
User <user@Da-Best-iMac.local>
2020-10-30 23:59:35 -0600

    Create table migration and define route

---
+++ crud-app/database/migrations/2020_10_31_045105_create_cats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name', 255);
+            $table->string('gender', 50);
+            $table->integer('age');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+            Schema::drop('cats');
+        });
+    }
+}
